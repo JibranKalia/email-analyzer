8fcc2a6cb6e65b9bd94e1ef6c9b890611dbc09cf
Jibran Kalia <jibran.kalia@gmail.com>
2024-05-01 12:01:50 -0500

    fix

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-     "email_analyzer/db"
-     "email_analyzer/routes"
-     "github.com/gin-gonic/gin"
+	"email_analyzer/db"
+	"email_analyzer/routes"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    db.InitDB() 
+	db.InitDB()
 
-    router := gin.Default() 
-    routes.RegisterRoutes(router) // Pass router to the registration function
+	router := gin.Default()
+	routes.RegisterRoutes(router) // Pass router to the registration function
 
-    router.Run(":8080") // Example port, adjust as needed
+	router.Run(":8080") // Example port, adjust as needed
 }
-
